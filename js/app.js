b740b4406db3365fccd266553685dd3725e3eaad
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2023-08-21 23:22:44 +0200

    add gallery and footer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
 import {showingAndHoningSearch, showingMobileMenu} from './header.js';
 import {movingCard} from './offer.js';
+import {gallery} from './gallery.js';
 
 document.addEventListener("DOMContentLoaded", function (){
     showingAndHoningSearch();
     showingMobileMenu();
     movingCard()
+    gallery()
+
 });
